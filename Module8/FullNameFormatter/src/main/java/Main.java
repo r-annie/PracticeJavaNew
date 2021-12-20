8da74accbd3ab8ce9e7b28571fdc6e2b3725e1d7
Annie <tarasova.ann@list.ru>
2021-12-20 23:12:32 +0400

    Added "FullNameFormatter"

--- Module8/FullNameFormatter/src/main/java/Main.java
+++ Module8/FullNameFormatter/src/main/java/Main.java
@@ -4,15 +4,54 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+          String input = scanner.nextLine();
+          if (input.equals("0")) {
+              break;
+          }
+          int count = 0;
+          boolean isCorrectLetter = false;
+
+          for (int i = 0; i < input.length(); i++) {
+              char currentChar = input.charAt(i);
+              if (currentChar == ' ') {
+                  count++;
+              }
+              if ((((int) 'А' <= (int) currentChar) && ((int) 'ё' >= (int) currentChar))
+                      || currentChar == '-' || currentChar == ' ') {
+                  isCorrectLetter = true;
+              } else {
+                  break;
+              }
+          }
+          if (isCorrectLetter && count == 2) {
+              System.out.println(getFormatting(input));
+          } else {
+              System.out.println("Введенная строка не является ФИО");
+          }
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-    }
   }
 
+    public static String getFormatting(String text) {
+        String nameSurname = text.trim();
+
+        int firstSpace = nameSurname.indexOf(" ");
+        int lastSpace = nameSurname.lastIndexOf(" ");
+
+        int startSurname = 0;
+        int endSurname = firstSpace;
+        String surname = nameSurname.substring(startSurname, endSurname);
+
+        int startName = endSurname + 1;
+        int endName = lastSpace;
+        String name = nameSurname.substring(startName, endName);
+
+        int startPatronymic = endName + 1;
+        String patronymic = nameSurname.substring(startPatronymic);
+
+        String template = "Фамилия: %s%nИмя: %s%nОтчество: %s";
+        String result = String.format(template, surname, name, patronymic);
+        return result;
+      }
 }
