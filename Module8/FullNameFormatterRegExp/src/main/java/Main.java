2df1a2643c466ce255450699702ce3cdf9e352d0
Annie <tarasova.ann@list.ru>
2021-12-24 03:17:10 +0400

    Added FullNameFormatterRegExp

--- Module8/FullNameFormatterRegExp/src/main/java/Main.java
+++ Module8/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -10,9 +12,34 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      System.out.println(getFormatting(input));
     }
   }
 
+  public static String getFormatting(String textOfInput) {
+    String nameSurname = textOfInput.trim();
+    String regex = "[A-zА-ё- ]+";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(nameSurname);
+    String result = "";
+    String textOfFullName = "";
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      textOfFullName = nameSurname.substring(start, end);
+    }
+    int count = 0;
+    for(int i = 0; i < textOfFullName.length(); i++) {
+      if(Character.isWhitespace(textOfFullName.charAt(i)))
+      count++;
+    }
+    if (count == 2) {
+      String[] text = textOfFullName.split(" ");
+      String template = "Фамилия: %s%nИмя: %s%nОтчество: %s";
+        result = String.format(template, text[0], text[1], text[2]);
+      } else {
+        result = "Введенная строка не является ФИО";
+      }
+    return result;
+  }
 }
