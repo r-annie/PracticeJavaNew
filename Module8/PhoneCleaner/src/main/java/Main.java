e919dd559e4fe72abed9354b78b1dcfe87160ffc
Annie <tarasova.ann@list.ru>
2021-12-24 03:43:24 +0400

    Added PhoneCleaner

--- Module8/PhoneCleaner/src/main/java/Main.java
+++ Module8/PhoneCleaner/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -10,8 +12,36 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+      System.out.println(formatPhoneNumber(input));
     }
   }
 
+  public static String formatPhoneNumber (String phoneNumber) {
+    String regex = "[^0-9]";
+    String regexDo = "[0-9]";
+    phoneNumber = phoneNumber.trim();
+    String phoneReplace = phoneNumber.replaceAll(regex,"");
+    Pattern pattern = Pattern.compile(regexDo);
+    Matcher matcher = pattern.matcher(phoneReplace);
+    int count = 0;
+    String result = "";
+    while (matcher.find())
+      count++;
+      if (count == 11) {
+        if (phoneReplace.startsWith("8")) {
+          phoneReplace = phoneReplace.substring(1);
+          result = "7" + phoneReplace;
+        }
+        if (phoneReplace.startsWith("7")) {
+          result = phoneReplace;
+        }
+      }
+      if (count == 10) {
+        result = "7" + phoneReplace;
+      }
+    if (result.equals("")) {
+      result = "Неверный формат номера";
+    }
+    return result;
+  }
 }
