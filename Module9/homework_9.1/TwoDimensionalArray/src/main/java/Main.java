314bb7739ac9f664d4acf86cfd41c98307d86c7a
Annie <tarasova.ann@list.ru>
2022-01-01 19:40:09 +0400

    Module 9.1, task 3 added.

--- Module9/homework_9.1/TwoDimensionalArray/src/main/java/Main.java
+++ Module9/homework_9.1/TwoDimensionalArray/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
+        System.out.println(arrayToString(TwoDimensionalArray.getTwoDimensionalArray(7)));
+    }
+
+    private static String arrayToString(char[][] array) {
+        return Arrays.stream(array)
+                .map(Arrays::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
--- Module9/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ Module9/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,14 +1,29 @@
 public class TwoDimensionalArray {
     public static char symbol = 'X';
+    public static char spaceSymbol = ' ';
 
     public static char[][] getTwoDimensionalArray(int size) {
-
+        char twoDim[][] = new char [size][size];
+        for (int i = 0; i < size / 2 + 1; i++) {
+            for (int j = 0; j < size / 2 + 1; j++) {
+                if (i == j) {
+                    twoDim[i][j] = symbol;
+                    twoDim[i][size - 1 - j] = symbol;
+                    twoDim[size - 1 - i][j] = symbol;
+                    twoDim[size - 1 - i][size - 1 - j] = symbol;
+                } else {
+                    twoDim[i][j] = spaceSymbol;
+                    twoDim[i][size - 1 - j] = spaceSymbol;
+                    twoDim[size - 1 - i][j] = spaceSymbol;
+                    twoDim[size - 1 - i][size - 1 - j] = spaceSymbol;
+                }
+            }
+        }
         //TODO: Написать метод, который создаст двумерный массив char заданного размера.
         // массив должен содержать символ symbol по диагоналям, пример для size = 3
         // [X,  , X]
         // [ , X,  ]
         // [X,  , X]
-
-        return new char[0][0];
+        return twoDim;
     }
 }
